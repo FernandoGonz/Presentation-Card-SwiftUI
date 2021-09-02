d77a06f482869f545ad05fc08982fe045a5f4403
Fernando Gonzalez <ing.fernandogonzalez@outlook.com>
2021-09-02 12:51:32 -0500

    Initial Commit

---
+++ FernandoCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FernandoCard
+//
+//  Created by Fernando González on 02/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
